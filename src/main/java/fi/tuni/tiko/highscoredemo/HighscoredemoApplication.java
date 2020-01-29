8c676f694cf71fb64d29a988b2730fd48ab0db3b
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-01-29 10:59:02 +0200

    Work on GET method.

--- src/main/java/fi/tuni/tiko/highscoredemo/HighscoredemoApplication.java
+++ src/main/java/fi/tuni/tiko/highscoredemo/HighscoredemoApplication.java
@@ -4,8 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
@@ -39,6 +46,18 @@ public class HighscoredemoApplication implements CommandLineRunner {
 
 		highScoreRepo.saveAll(highScores);
 	}
+
+	@RequestMapping(value = "add/", method = RequestMethod.POST)
+	public ResponseEntity<Void> add(@RequestBody HighScoreEntry c, UriComponentsBuilder b) {
+		highScoreRepo.save(c);
+		UriComponents uriComponents = b.path("/get/{id}").buildAndExpand(c.getId());
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(uriComponents.toUri());
+
+		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	}
+
 
 	@RequestMapping("get/")
 	public Iterable<HighScoreEntry> getScores() {
